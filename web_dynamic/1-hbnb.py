9fc8a73f157eb9ce8a0024c669d5b6c4b2d6140a
hagarSamy <hs1800@fayoum.edu.eg>
2024-06-01 19:27:01 +0300

    little update to 1-hbnb

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -21,7 +21,8 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+# replaced the route 0-hbnb with 1-hbnb 
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
